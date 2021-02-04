1c0ca26b033b967adfca0ebc695a7bc0586328fb
MasonTao <yingchun_tao@163.com>
2021-02-04 17:06:37 +0800

    feat: build framework

---
+++ razzle.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  // modifyWebpackConfig(opts) {
+  //   const config = opts.webpackConfig;
+  //   config.resolve.alias = {
+  //     '~containers': path.resolve(__dirname, 'src/containers')
+  //   };
+  //   return config;
+  // },
+};
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Router from "./containers/router/routes";
+import "./App.css";
+
+const App = () => <Router></Router>;
+
+export default App;
